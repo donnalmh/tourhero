fca67164383c5ecb83f0efe4dfdc2f7b13042517
Donna Samuel <donnasamuel@gmail.com>
2024-02-23 20:04:42 +0200

    add itinerary title

--- src/components/content/main/ItineraryDetails.vue
+++ src/components/content/main/ItineraryDetails.vue
@@ -3,11 +3,15 @@
 
 <template>
     <div class="details bg-white">
-        <div class="flex justify-between light-gray">
+        <div class="flex justify-between caption">
             <div>Asia > Singapore > Strenuous Travel Style</div>
             <div>ID: 09781021</div>
         </div>
         <div class="title mt-4">Marina Bay Sands Observation</div>
+        <div>A great spot for R&R: order yourself a drink from the bar and get comfortable on one of the day beds
+            that line the impressive 100-metre pool.
+        </div>
+        <div class="itinerary-title mt-4">Itinerary</div>
 
     </div>
 </template>
@@ -21,12 +25,19 @@
         font: "Gilroy", sans-serif;
     }
 
-    .light-gray {
-        color: #B2B2B2;
+    .caption{
+        color: #A19A9A;
+        font-size: 0.8rem;
     }
     
     .title {
         font-weight: 900;
         font-size: 2em;
+    }
+
+    .itinerary-title {
+        color: #123162;
+        font-size: 1.4rem;
+        font-weight: 800;
     }
 </style>
