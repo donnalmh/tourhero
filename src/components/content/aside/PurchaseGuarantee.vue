fb063ca0952342ca33962c1a4748c7f5c04befa0
Donna Samuel <donnasamuel@gmail.com>
2024-02-29 15:44:43 +0200

    re-factor code

--- src/components/content/aside/PurchaseGuarantee.vue
+++ src/components/content/aside/PurchaseGuarantee.vue
@@ -1,14 +1,20 @@
+<script setup lang="ts">
+    const guaranteePoints: string[] = [
+        '0% Instalment Payment',
+        '$200 Deposit',
+        '24/7 Support',
+        'No Booking Fee',
+        'Free Cancellation & changes up to 45 days prior'
+        ]
+</script>
 <template>
 <div class="large-device:border large-device:border-slate-200 large-device:p-4 text-cerulean-blue rounded sm:mt-4 sm:pt-4">
     <div class=" sm:text-center sm:text-xl sm:mt-2"><i class="pi pi-list mr-3"></i><span class='navy confidence'>Buy With Confidence</span></div>
-
     <div class="points mt-3">
-    <div><i class="pi pi-check-circle mr-3"></i><span class='detail'>0% Instalment Payment</span></div>
-    <div><i class="pi pi-check-circle mr-3"></i><span class='detail'>$200 Deposit</span></div>
-    <div><i class="pi pi-check-circle mr-3"></i><span class='detail'>24/7 Support</span></div>
-    <div><i class="pi pi-check-circle mr-3"></i><span class='detail'>No Booking Fee</span></div>
-    <div><i class="pi pi-check-circle mr-3"></i><span class='detail'>Free Cancellation & changes up to 45 days prior</span></div>
+    <div v-for="(point, index) in guaranteePoints" :key="index">
+        <div><i class="pi pi-check-circle mr-3"></i><span class='detail'>{{ point }}</span></div>
     </div>
+</div>
 
 </div>
 
